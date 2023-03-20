daec5b27783d1c726096e1c01e4961121ab011b1
omkadam26 <97826707+omkadam26@users.noreply.github.com>
2023-03-21 00:07:48 +0530

    Add files via upload

---
+++ ToggleButton/script.js
@@ -0,0 +1,33 @@
+var slidder = document.getElementById("btn");
+var isToggle = false;
+var b = document.getElementsByTagName("body")[0];
+
+function toggle()
+{
+
+	console.log("Clicked");
+	if(isToggle)
+	{
+		toggleWhite();
+		isToggle = false;
+	}
+	else
+	{
+		toggleBlack();
+		isToggle = true;
+	}
+
+
+}
+function toggleBlack(){
+    	
+		document.getElementsByClassName("circle")[0].classList.add("toggleCircle");
+		document.getElementsByClassName("oval")[0].classList.add("toggleOval");
+		document.getElementsByTagName("body")[0].classList.add("toggleBack");
+
+}
+function toggleWhite(){
+   	document.getElementsByClassName("circle")[0].classList.remove("toggleCircle");
+	document.getElementsByClassName("oval")[0].classList.remove("toggleOval");
+	document.getElementsByTagName("body")[0].classList.remove("toggleBack");
+}
